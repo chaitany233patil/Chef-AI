6b37681bc9f6e46b307c9307ed477727cfe7ac15
Chaitany233patil <patilchaitany529@gmail.com>
2025-01-07 12:07:13 +0530

    update chef-logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,8 @@
 export default function Header() {
   return (
     <header>
-      <img
-        src="/src/assets/kitchen-set-solid.svg"
-        alt="chef logo"
-        className="chef-logo"
-      />
-      <span className="app-title">ChefAI</span>
+      <img src="/kitchen-set-solid.svg" alt="chef logo" className="chef-logo" />
+      <span className="app-title">Chef-AI</span>
     </header>
   );
 }
